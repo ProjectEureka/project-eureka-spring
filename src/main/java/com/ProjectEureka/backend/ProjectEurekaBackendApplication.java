2f83cccff555eb0eeeaaf234e419bc2777a7b716
davidquines <davidsquines@gmail.com>
2020-11-22 18:37:17 -0800

    init spring files

---
+++ src/main/java/com/ProjectEureka/backend/ProjectEurekaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectEureka.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEurekaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEurekaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectEureka/backend/ProjectEurekaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectEureka.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEurekaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
