f7c44643421563d32746d8862f73c05dc0424c01
ayeejane <jane.cabanayan.463@my.csun.edu>
2021-01-17 20:15:02 -0800

    Merged with main

--- src/main/java/com/ProjectEureka/backend/models/User.java
+++ src/main/java/com/ProjectEureka/backend/models/User.java
@@ -2,7 +2,6 @@ package com.ProjectEureka.backend.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
 
 @Document(collection = "Users")
@@ -12,10 +11,25 @@ public class User {
     private String id;
     private String firstName;
     private String lastName;
+    private String firebaseUuid;
+    private String email;
+    private String city;
+    private String category;
+    private String pictureUrl;
+    private boolean role;
+    private int rating;
 
-    public User(String firstName, String lastName) {
+    public User(String firstName, String lastName, String firebaseUuid, String email, String city, String category, String pictureUrl, Boolean role,
+            Integer rating)) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.firebaseUuid = firebaseUuid;
+        this.email = email;
+        this.city = city;
+        this.category = category;
+        this.pictureUrl = pictureUrl;
+        this.role = role;
+        this.rating = rating;
     }
 
 }
--- src/main/java/com/ProjectEureka/backend/repositories/UserRepository.java
+++ src/main/java/com/ProjectEureka/backend/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.ProjectEureka.backend.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import com.ProjectEureka.backend.models.User;
 
 @Repository
@@ -10,4 +9,18 @@ public interface UserRepository extends MongoRepository<User, String> {
     public User findByFirstName(String firstName);
 
     public User findByLastName(String lastName);
+
+    public User findByFirebaseUuid(String firebaseUuid);
+
+    public User findByEmail(String email);
+
+    public User findByCity(String city);
+
+    public User findByCategory(String category);
+
+    public User findByPictureUrl(String pictureUrl);
+
+    public User findByRole(Boolean role);
+
+    public User findByRating(Integer rating);
 }
