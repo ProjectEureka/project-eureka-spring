7e8de1346ce11a0ebe59ef798c3328414a308212
Tony <txt020@gmail.com>
2021-03-24 17:30:08 -0700

    PE-59 | Created a view model  for Deleted  Users

---
+++ src/main/java/com/ProjectEureka/backend/services/DeleteUserDataService.java
@@ -0,0 +1,7 @@
+package com.ProjectEureka.backend.services;
+
+import com.ProjectEureka.backend.models.User;
+
+public interface DeleteUserDataService {
+    User deleteUserData(String id);
+}
---
+++ src/main/java/com/ProjectEureka/backend/services/DeleteUserDataServiceImpls.java
@@ -0,0 +1,43 @@
+package com.ProjectEureka.backend.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.ProjectEureka.backend.exception.ResourceNotFoundException;
+import com.ProjectEureka.backend.models.User;
+import com.ProjectEureka.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteUserDataServiceImpls implements DeleteUserDataService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User deleteUserData(String id){
+
+        Optional<User> user = this.userRepository.findById(id);
+
+
+        if(user.isPresent()){
+            User deleteUser = user.get();
+
+            deleteUser.setFirstName("deleted");
+            deleteUser.setLastName("deleted");
+            deleteUser.setEmail("deleted");
+            deleteUser.setCity("deleted");
+            deleteUser.setCategory("deleted");
+            deleteUser.setPictureUrl("deleted");
+            deleteUser.setRole(-1);
+            deleteUser.setRatings(new ArrayList<Integer>());
+            deleteUser.setAverageRating(0.0);
+
+            return deleteUser;
+        }
+        else{
+            throw new ResourceNotFoundException("User not found with id : " + id);
+        }
+    }   
+}
